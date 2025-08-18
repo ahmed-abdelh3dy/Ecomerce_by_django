868701e5025a2af9bec126551825b88b23c552ba
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-08-18 16:11:17 +0200

    refactore code of permissions

--- Ecomerce_by_django/products/permissions.py
+++ Ecomerce_by_django/products/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission , SAFE_METHODS
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS: 
+        if request.method in SAFE_METHODS: 
             return request.user.is_authenticated
         return request.user.is_authenticated and getattr(request.user, "role", None) == "admin"
